3d97dbca69783ee7c3264a131299098d9d44e5bc
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-12 22:24:58 +0000

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DDLC_ModManager
+namespace DDLC_ModManager.src
 {
-    internal static class Program
+    static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
