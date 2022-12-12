a3f504934fdc4a7fa55fb747e9d9c740db448f16
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-12 22:39:55 +0000

    Add files via upload

---
+++ src/Game.cs
@@ -0,0 +1,27 @@
+﻿using DDLC_ModManager.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDLC_ModManager.src
+{
+    public static class Games
+    {
+        public static Game INSTANCE = new Game();
+        public static Game DokiDokiLiteratureClub = new Game()
+        {
+            GAME_NAME = "DokiDokiLiteratureClub",
+        };
+
+        public class Game
+        {
+            public string GAME_NAME = "NULL";
+            public string EXE_NAME = string.Empty;
+        }
+    }
+}
