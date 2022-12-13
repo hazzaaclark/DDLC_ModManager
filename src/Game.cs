7bed306350539deaa965c803a8d505ed73feb97e
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-13 01:12:03 +0000

    Update Game.cs

--- src/Game.cs
+++ src/Game.cs
@@ -16,11 +16,17 @@ namespace DDLC_ModManager.src
         public static Game DokiDokiLiteratureClub = new Game()
         {
             GAME_NAME = "DokiDokiLiteratureClub",
+            EXE_NAME = "DokiDokiLiteratureClub.exe",
+            FOLDERS = new[] { "game", "lib" },
+            GAME_PATH = Path.Combine("DokiDokiLiteratureClub", "")
         };
 
         public static Game DokiDokiLiteratureClub_Plus = new Game()
         {
             GAME_NAME = "DokiDokiLiteratureClub_Plus",
+            EXE_NAME = ".exe",
+            FOLDERS = new[] { "game", "lib" },
+            GAME_PATH = Path.Combine("", "")
         };
 
         public static IEnumerable<Game> SUPPORTED_GAMES()
@@ -32,6 +38,7 @@ namespace DDLC_ModManager.src
         public class Game
         {
             public string GAME_NAME = "NULL";
+            public string GAME_PATH = string.Empty;
             public string EXE_NAME = string.Empty;
             public Int32 DIRECT_X_VER = Int32.MaxValue;
             public string[] FOLDERS = {"raw"};
@@ -40,6 +47,13 @@ namespace DDLC_ModManager.src
             {
                 return GAME_NAME;
             }
+        }
+
+        public enum GameLaunch
+        {
+            STEAM,
+            EPIC,
+            NULL
         }
     }
 }
