6f87151cc3bb7799d7570a88939e4e23a85369bf
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-16 18:53:09 +0000

    Update MainForm.Designer.cs

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -16,13 +16,20 @@ namespace DDLC_ModManager
         #region Windows Form Designer generated code
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Doki Doki Mod Manager";
+            this.Load += new System.EventHandler(this.FORM_LOAD);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
